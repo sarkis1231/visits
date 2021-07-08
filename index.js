5f38811e0ebe5c10c802ed2887fd74d2b2df9e69
sarkis1231 <sakooghly@gmail.com>
2021-07-08 12:23:54 +0400

    Inital commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const redis = require("redis");
+
+
+const PORT = 8081
+const app = express();
+
+// setup redis client 
+const client = redis.createClient();
+client.set("visits", 0);
+
+app.get("/", (req, res) => {
+    client.get("visits", (err, visits) => {
+        res.send(`Number of visits is ${visits}`);
+        client.set("visits", +visits + 1);
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
